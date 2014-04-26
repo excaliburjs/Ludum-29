70396d4721178df24fd6967227f1c68de6a20e79
Josh Edeen <josh.edeen@gmail.com>
2014-04-26 00:33:29 -0500

    base level implementation

---
+++ Game/game/Level.ts
@@ -0,0 +1,40 @@
+﻿/// <reference path="../scripts/Excalibur.d.ts" />
+
+export class BaseLevel extends ex.Scene implements ex.ILoadable {
+    private _isLoaded: boolean = false;
+    public data: any;
+
+    constructor(public jsonPath: string, public spriteSheet: ex.SpriteSheet) {
+        super();
+    }
+
+    public onInitialize(engine: ex.Engine) {
+        //build the collision map
+    }
+
+    //TODO overload draw: draw HUD, UI, etc.; also draw the collision map
+    // remember to call super.draw()
+
+    public load(): ex.Promise<any> {
+        var complete = new ex.Promise<any>();
+        var request = new XMLHttpRequest();
+        request.open("GET", this.path, true);
+        request.responseType = "json";
+        request.onload = (e) => {
+            complete.resolve(request.response);
+            this.data = request.response;
+        };
+         request.send();
+         return complete;
+     }
+
+     public isLoaded(): boolean {
+         return this._isLoaded;
+     }
+
+     public onprogress: (e: any) => void = () => { };
+
+     public oncomplete: () => void = () => { };
+
+     public onerror: (e: any) => void = () => { };
+ }
--- Game/game/game.ts
+++ Game/game/game.ts
@@ -2,4 +2,7 @@
 
 var game = new ex.Engine();
 
+var kraken = new ex.Actor(200, 200, 100, 100, ex.Color.Red);
+game.addChild(kraken);
+
 game.start();
