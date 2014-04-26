b58c3b43c9b0bc28e84748a13fce615189466683
Kamran Ayub <kamran.ayub@gmail.com>
2014-04-26 07:58:18 -0500

    Terrain (basic) and level zero

---
+++ Assets/Tilesets/World.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="World" tilewidth="48" tileheight="48">
+ <image source="../../Game/images/tilesets/world-48x48.png" width="2732" height="2014"/>
+</tileset>
