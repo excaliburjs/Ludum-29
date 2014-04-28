8de49fb2aa42e589709404d8f9dc4a70850ed732
Erik Onarheim <erik.onarheim@gmail.com>
2014-04-27 21:48:31 -0500

    Fix merge

--- Game/game/Config.ts
+++ Game/game/Config.ts
@@ -3,23 +3,15 @@
     // Kraken
     static defaultKrakenWidth: number = 50;
     static defaultKrakenHeight: number = 50;
-   // Kraken
-   static defaultKrakenWidth: number = 150;
-   static defaultKrakenHeight: number = 50;
     static defaultKrakenSpeedScale: number = 10;
     static defaultKrakenHealth: number = 100;
     //static defaultKrakenStamina: number = 100;
     static defaultKrakenSpecial: number = 5;
-   static defaultKrakenHealth: number = 100;
    //static defaultKrakenStamina: number = 100;
    //static defaultKrakenSpecial: number = 5;
     static krakenAttackRange: number = 150;
     static krakenInertiaDampen: number = 1;
     
-    // Enemies
-    static defaultEnemyWidth: number = 100;
-    static defaultEnemyHeight: number = 100;
-
    // Enemies
    static defaultEnemyWidth: number = 191;
    static defaultEnemyHeight: number = 73;
