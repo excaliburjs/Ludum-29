11cfcae0a3dd7ba22d6f362f5b9863257e678897
Erik Onarheim <erik.onarheim@gmail.com>
2014-04-28 19:19:22 -0500

    Add relative paths to resources

--- Game/game/Resources.ts
+++ Game/game/Resources.ts
@@ -4,41 +4,41 @@
 var Resources = {
 
    // Soundtrack
-   SoundTrack: new ex.Sound("/sounds/airwave mit mitch kelli.mp3"),
+   SoundTrack: new ex.Sound("sounds/airwave mit mitch kelli.mp3"),
 
    // Levels
-   Level0: new BaseLevel("/maps/Level-0.json"),
+   Level0: new BaseLevel("maps/Level-0.json"),
 
    // Textures
-   SplashTexture: new ex.Texture("/images/splash.jpg"),
-   DeathTexture: new ex.Texture("/images/death.jpg"),
-   VictoryTexture: new ex.Texture("/images/victory.jpg"),
-   KrakenTexture: new ex.Texture("/images/kraken/KrakenSpriteSheet.png"),
-   StartButtonTexture: new ex.Texture("/images/start.png"),
+   SplashTexture: new ex.Texture("images/splash.jpg"),
+   DeathTexture: new ex.Texture("images/death.jpg"),
+   VictoryTexture: new ex.Texture("images/victory.jpg"),
+   KrakenTexture: new ex.Texture("images/kraken/KrakenSpriteSheet.png"),
+   StartButtonTexture: new ex.Texture("images/start.png"),
 
    // Enemies
-   Ship1Texture: new ex.Texture("/images/ship-1.png"),
+   Ship1Texture: new ex.Texture("images/ship-1.png"),
 
-   BulletTexture: new ex.Texture("/images/bullet.png"),
+   BulletTexture: new ex.Texture("images/bullet.png"),
 
-   AlertTexture: new ex.Texture("/images/alert.png"),
+   AlertTexture: new ex.Texture("images/alert.png"),
 
-   BulletSound: new ex.Sound("/sounds/shoot.wav"),
+   BulletSound: new ex.Sound("sounds/shoot.wav"),
 
-   SinkSound: new ex.Sound("/sounds/shipsink.wav"),
+   SinkSound: new ex.Sound("sounds/shipsink.wav"),
 
    // Tilesets
-   TerrainTexture: new ex.Texture("/images/tilesets/terrain.png"),
+   TerrainTexture: new ex.Texture("images/tilesets/terrain.png"),
 
    // Kraken
-   SoundSwim: new ex.Sound("/sounds/swim.wav"),
-   SoundHurt: new ex.Sound("/sounds/hurt.wav"),
-   SoundWaves: new ex.Sound("/sounds/waves.wav"),
-   SoundDeath: new ex.Sound("/sounds/death.wav"),
-   HitSound: new ex.Sound("/sounds/hit.wav"),
+   SoundSwim: new ex.Sound("sounds/swim.wav"),
+   SoundHurt: new ex.Sound("sounds/hurt.wav"),
+   SoundWaves: new ex.Sound("sounds/waves.wav"),
+   SoundDeath: new ex.Sound("sounds/death.wav"),
+   HitSound: new ex.Sound("sounds/hit.wav"),
 
    // HUD
-   Heart: new ex.Texture("/images/heart.png")
+   Heart: new ex.Texture("images/heart.png")
 };
 
 var Palette = {
